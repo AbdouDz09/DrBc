72d60864eccebfe9b1f67925b98b529a67c9d1b3
AbdouDz09 <45575757+AbdouDz09@users.noreply.github.com>
2019-01-13 18:15:02 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -198,7 +198,7 @@ client.on("message", async message => {
             معلومات عن السيرفر : ${prefix}server
             برودكاست للأونلاين فقط : ${prefix}bco
             يعرض لك عدد المتبندين من سيرفرك : ${prefix}banned
-            رابط سيرفر الدعم الفني : https://discord.gg/YEXcDXt 
+            رابط سيرفر الدعم الفني : https://discord.gg/PpXPPZ9
             **`);
             message.channel.sendEmbed(help); // رابط السيرفر يعود الى سيرفر CODES .
     }
